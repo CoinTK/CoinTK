c2a9512f25d8171a6c9149dd31c712d8e5c7c213
wanqizhu <1213.ghs@gmail.com>
2017-02-25 16:04:35 -0600

    Included the plot results of backtest/naive.py to the git repo

--- backtests/naive.py
+++ backtests/naive.py
@@ -2,4 +2,4 @@ from cointk.backtest import backtest
 from cointk.strategies import NaiveStrategy
 
 strategy = NaiveStrategy(n_prices=1000, threshold=0.8)
-backtest(strategy, plot_name='Naive-full-threshhold-0.8.html', verbose=1)
+backtest(strategy, plot_name='plots/Naive-full-threshhold-0.8.html', verbose=1)
--- backtests/naive_test.py
+++ backtests/naive_test.py
@@ -3,4 +3,4 @@ from cointk.strategies import OldNaiveStrategy
 
 strategy = OldNaiveStrategy(n_prices=100, threshold=0.8)
 # Use the 100k test dataset
-backtest(strategy, data_fnm='data/coinbaseUSD_100k_sample.npz', plot_name='Naive-test-100k-threshhold-0.8.html', verbose=1)
+backtest(strategy, data_fnm='data/coinbaseUSD_100k_sample.npz', plot_name='plots/Naive-test-100k-threshhold-0.8.html', verbose=1)
