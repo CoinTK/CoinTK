ca1a3e734f862ba67d12c22124e2d24052411e18
Alex Gajewski <agajews@gmail.com>
2017-02-25 21:25:50 -0600

    Renamed to cointk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(
-    name='an_example_pypi_project',
+    name='CoinTK',
     version='0.0.1',
     author='Alex Gajewski, Wanqi Zhu, Ashwin Aggarwal',
     author_email='agajews@gmail.com, 1213.ghs@gmail.com, aaggarw99@gmail.com',
